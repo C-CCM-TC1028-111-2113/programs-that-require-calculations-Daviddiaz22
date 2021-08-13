c880ea2eb9ffcc007c2c732ddc74fe29a1948bdb
Daviddiaz22 <88851000+Daviddiaz22@users.noreply.github.com>
2021-08-13 15:06:23 -0500

    Update exercise.py

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,14 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
-number=int(input('Dame un número:'))
-num=int(input('Dame un múmero:'))
+number=int(input('Dame un número: '))
+num=int(input('Dame un múmero: '))
 suma=number+num
 resta=number-num
 multi=number*num
 
-print('Suma:'+str(suma))
-print('Resta:'+str(resta))
-print('Multiplicación:'+str(multi))
+print('Suma: '+str(suma))
+print('Resta: '+str(resta))
+print('Multiplicación: '+str(multi))
 
 
 if __name__ == '__main__':
