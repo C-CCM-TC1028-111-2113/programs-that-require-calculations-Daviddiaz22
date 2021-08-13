b86a076783c12f5ee9466e81a278390a3b197b96
Daviddiaz22 <88851000+Daviddiaz22@users.noreply.github.com>
2021-08-13 15:04:24 -0500

    Update exercise.py

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,10 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-num1=int(input('Dame un número: '))
-num2=int(input('Dame un múmero: '))
-suma=num1+num2
-resta=num1-num2
-multi=num1*num2
+number=int(input('Dame un número:'))
+num=int(input('Dame un múmero:'))
+suma=number+num
+resta=number-num
+multi=number*num
 
 print('Suma:'+str(suma))
 print('Resta:'+str(resta))
