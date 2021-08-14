8b30d86d9927ee027fe346da79b3bc7aaa8ea302
Daviddiaz22 <88851000+Daviddiaz22@users.noreply.github.com>
2021-08-13 20:23:25 -0500

    Update exercise.py

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,12 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-    base=int(input('Dame la base: '))
-altura=int(input('Dame la altura:'))
+    base=float(input('Dame la base:'))
+altura=float(input('Dame la altura:'))
 
 area=(base*altura)/2
 
-print('El área es: '+str(area))
-
+print('El área es:'+str(area))
 
 if __name__ == '__main__':
     main()
